3391bc1068b6bcb59091f62b74007f84a2fd51b8
William Paul McInnis <william.p.mcinnis4.mil@army.mil>
2025-09-20 15:39:11 -0400

    Reducing mccabe score :)

--- ERP_Data_Engineering/src/OBI_Downloads.py
+++ ERP_Data_Engineering/src/OBI_Downloads.py
@@ -1,10 +1,6 @@
 import time
-import datetime
-import shutil
-import os
-import sys
 
-from helper import day_of_week_check, get_user_creds
+from helper import get_user_creds
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions 
@@ -122,6 +118,61 @@ class DownloadFilesfromOBI:
         driver.close()
         driver.quit()     
             
+    def download_files_mon(self, attachment_rate_urls: dict, driver: webdriver.Firefox) -> None:
+        if self.weekday != 0:
+            raise TypeError("Wrong Day of Week")
+        
+        for URL in attachment_rate_urls:
+            
+            driver.get(URL)
+            export_xpath = '/html/body/div[2]/div/table[1]/tbody/tr[1]/td[2]/div/table[1]/tbody/tr/td[2]/div/table/tbody/tr/td/div/table/tbody/tr/td[7]'
+
+            WebDriverWait(driver, 20).until(expected_conditions.element_to_be_clickable((By.XPATH, export_xpath))).click()
+            data_xpath = '/html/body/div[3]/table/tbody/tr[1]/td[1]/a[5]/table/tbody/tr/td[2]'
+            driver.find_element(By.XPATH, data_xpath).click()
+            csv_xpath = '/html/body/div[4]/table/tbody/tr[1]/td[1]/a[2]/table/tbody/tr/td[2]'
+            driver.find_element(By.XPATH, csv_xpath).click()
+            time.sleep(5)
+
+            if 'Anakkar' in URL:
+                excel_xpath = '/html/body/div[3]/table/tbody/tr[1]/td[1]/a[2]/table'
+                driver.find_element(By.XPATH, excel_xpath).click()
+                
+
+    def failed_thurs_download(self, current_position: int, reference_data_urls: dict, driver: webdriver.Firefox) -> None:
+        for num in range(current_position, len(reference_data_urls)-1):
+            URL = reference_data_urls[num]
+            driver.get(URL)
+            export_xpath = '/html/body/div[2]/div/table[1]/tbody/tr[1]/td[2]/div/table[1]/tbody/tr/td[2]/div/table/tbody/tr/td/div/table/tbody/tr/td[7]'
+            WebDriverWait(driver, 120).until(expected_conditions.element_to_be_clickable((By.XPATH, export_xpath))).click()
+            data_xpath = '/html/body/div[3]/table/tbody/tr[1]/td[1]/a[5]/table/tbody/tr/td[2]'          
+            driver.find_element(By.XPATH, data_xpath).click()
+            csv_xpath = '/html/body/div[4]/table/tbody/tr[1]/td[1]/a[2]/table/tbody/tr/td[2]'
+            driver.find_element(By.XPATH, csv_xpath).click()
+            if 'regis' in URL.lower():
+                time.sleep(15)
+            time.sleep(5)
+
+
+    def download_files_thurs(self, reference_data_urls: dict, driver: webdriver.Firefox) -> None:
+        try:
+            for URL in reference_data_urls:
+                driver.get(URL)
+                export_xpath = '/html/body/div[2]/div/table[1]/tbody/tr[1]/td[2]/div/table[1]/tbody/tr/td[2]/div/table/tbody/tr/td/div/table/tbody/tr/td[7]'
+                WebDriverWait(driver, 120).until(expected_conditions.element_to_be_clickable((By.XPATH, export_xpath))).click()
+                data_xpath = '/html/body/div[3]/table/tbody/tr[1]/td[1]/a[5]/table/tbody/tr/td[2]'          
+                driver.find_element(By.XPATH, data_xpath).click()
+                csv_xpath = '/html/body/div[4]/table/tbody/tr[1]/td[1]/a[2]/table/tbody/tr/td[2]'
+                driver.find_element(By.XPATH, csv_xpath).click()
+                if 'regis' in URL.lower():
+                    time.sleep(30)
+                time.sleep(5)
+
+        except TimeoutException:
+            current_position = reference_data_urls.index(URL)
+            time.sleep(600)
+            self.failed_thurs_download(current_position=current_position, reference_data_urls=reference_data_urls, driver=driver)
+            
 
     def download_files(self):
         """This function takes the driver from the OBI Login and then checks the day of the week to determine
@@ -144,143 +195,19 @@ class DownloadFilesfromOBI:
 
         
         if day_of_week == 0:
-            for URL in attachment_rate_urls:
-                
-                driver.get(URL)
-                export_xpath = '/html/body/div[2]/div/table[1]/tbody/tr[1]/td[2]/div/table[1]/tbody/tr/td[2]/div/table/tbody/tr/td/div/table/tbody/tr/td[7]'
-
-                WebDriverWait(driver, 20).until(expected_conditions.element_to_be_clickable((By.XPATH, export_xpath))).click()
-
-                if 'Anakkar' in URL:
-                    excel_xpath = '/html/body/div[3]/table/tbody/tr[1]/td[1]/a[2]/table'
-                    driver.find_element(By.XPATH, excel_xpath).click()
-
-                else:
-                    data_xpath = '/html/body/div[3]/table/tbody/tr[1]/td[1]/a[5]/table/tbody/tr/td[2]'
-                    driver.find_element(By.XPATH, data_xpath).click()
-                    csv_xpath = '/html/body/div[4]/table/tbody/tr[1]/td[1]/a[2]/table/tbody/tr/td[2]'
-                    driver.find_element(By.XPATH, csv_xpath).click()
-                time.sleep(5)
-        try:
-            for URL in reference_data_urls:
-                driver.get(URL)
-                export_xpath = '/html/body/div[2]/div/table[1]/tbody/tr[1]/td[2]/div/table[1]/tbody/tr/td[2]/div/table/tbody/tr/td/div/table/tbody/tr/td[7]'
-                WebDriverWait(driver, 120).until(expected_conditions.element_to_be_clickable((By.XPATH, export_xpath))).click()
-                data_xpath = '/html/body/div[3]/table/tbody/tr[1]/td[1]/a[5]/table/tbody/tr/td[2]'          
-                driver.find_element(By.XPATH, data_xpath).click()
-                csv_xpath = '/html/body/div[4]/table/tbody/tr[1]/td[1]/a[2]/table/tbody/tr/td[2]'
-                driver.find_element(By.XPATH, csv_xpath).click()
-                if 'regis' in URL.lower():
-                    print('Registration!!')
-                    time.sleep(30)
-                    print('Done sleeping!')
-                time.sleep(5)
-
-        except TimeoutException:
-            current_position = reference_data_urls.index(URL)
-            print("Timed out!")
-            print("Trying again!")
-            time.sleep(600)
-            for num in range(current_position, len(reference_data_urls)-1):
-                URL = reference_data_urls[num]
-                driver.get(URL)
-                export_xpath = '/html/body/div[2]/div/table[1]/tbody/tr[1]/td[2]/div/table[1]/tbody/tr/td[2]/div/table/tbody/tr/td/div/table/tbody/tr/td[7]'
-                WebDriverWait(driver, 120).until(expected_conditions.element_to_be_clickable((By.XPATH, export_xpath))).click()
-                data_xpath = '/html/body/div[3]/table/tbody/tr[1]/td[1]/a[5]/table/tbody/tr/td[2]'          
-                driver.find_element(By.XPATH, data_xpath).click()
-                csv_xpath = '/html/body/div[4]/table/tbody/tr[1]/td[1]/a[2]/table/tbody/tr/td[2]'
-                driver.find_element(By.XPATH, csv_xpath).click()
-                if 'regis' in URL.lower():
-                    print('Registration!!')
-                    time.sleep(15)
-                    print('Done sleeping!')
-                time.sleep(5)
-
-
-        print('Loop end!')
+            self.download_files_mon(attachment_rate_urls=attachment_rate_urls, driver=driver)
+            self.download_files_thurs(reference_data_urls=reference_data_urls, driver=driver)
+            driver.close()
+            driver.quit()
+            return None
+        
+        self.download_files_thurs(reference_data_urls=reference_data_urls, driver=driver)
         driver.close()
         driver.quit()
-        print("Driver Quit!")
         return None
-
-    def check_files_downloaded(self):
-        """This function conducts an error check to ensure all files required are downloaded.
-        If there are files missing then the next function redownloads them"""
         
-        day_of_week = self.weekday[0]
-        if day_of_week not in (0,3):
-            print('Not a Monday or Thursday, come back then!')
-            return None
-
-        print(f'Day of week is {day_of_week}')
-        attachment_rate_urls = [r'http://ccinsight.internal.clubcar.com:9502/analytics/saw.dll?PortalGo&Action=prompt&path=%2Fshared%2FSFA%20Reports%2FOnward%206P%20by%20date',
-                                    r'http://ccinsight.internal.clubcar.com:9502/analytics/saw.dll?PortalGo&Action=prompt&path=%2Fshared%2Fcc%20Service%20Parts%2FAnakkar%2FOnward%20Att.%20Rate%2FOnward%20Accessories%20-%202023%20Attachment%20Rate']
-
-        reference_data_urls = [r'http://ccinsight.internal.clubcar.com:9502/analytics/saw.dll?PortalGo&Action=prompt&path=%2Fshared%2FSFA%20Reports%2FPLAN%20ETL%20-%20Objectives%20Data',
-                                r'http://ccinsight.internal.clubcar.com:9502/analytics/saw.dll?PortalGo&Action=prompt&path=%2Fshared%2FSFA%20Reports%2FPLAN%20ETL%20-%20Plan%20Data',
-                                r'http://ccinsight.internal.clubcar.com:9502/analytics/saw.dll?PortalGo&Action=prompt&path=%2Fshared%2FSFA%20Reports%2FWarr%20Reg%20Flag%20CY',
-                                r'http://ccinsight.internal.clubcar.com:9502/analytics/saw.dll?PortalGo&Action=prompt&path=%2Fshared%2FSFA%20Reports%2FWarranty%20Accepted%20Amount%20CY%20for%20SFDC%20updates',
-                                r'http://ccinsight.internal.clubcar.com:9502/analytics/saw.dll?PortalGo&Action=prompt&path=%2Fshared%2FSFA%20Reports%2FWarranty%20Registrations%20for%20SFDC%20updates',
-                                r'http://ccinsight.internal.clubcar.com:9502/analytics/saw.dll?PortalGo&Action=prompt&path=%2Fshared%2FSFA%20Reports%2FOrder%20-%20Details%20for%20WR%20CY']
-
-        attachment_rate_dict = {0:'Onward 6P by date.csv', 1: 'Onward Accessories - 2023 Attachment Rate.xlsx'}
-        reference_data_dict = {0:'PLAN ETL - Objectives Data.csv', 1:'PLAN ETL - Plan Data.csv', 2: 'Warr Reg Flag CY.csv', 3: 'Warranty Accepted Amount CY for SFDC updates.csv', 4:'Warranty Registrations for SFDC updates.csv', 5: 'Order - Details for WR CY.csv'}
-
-        downloads = os.listdir(r'C:\Users\wxm3287\Downloads')
-        csvs_xlsxs = [file for file in downloads if '.csv' in file or '.xlsx' in file]
-    
-        
-        reference_data_missed_urls = [reference_data_urls[position] for position, file in reference_data_dict.items() if file not in csvs_xlsxs]
-        print('Created missing list! ')
-        if day_of_week == 0:
-            attachment_rate_missed_urls = [attachment_rate_urls[position] for position, file in attachment_rate_dict.items() if file not in csvs_xlsxs]
-            missed_urls = attachment_rate_missed_urls + reference_data_missed_urls
-        
-        else:
-            missed_urls = reference_data_missed_urls
-        
-        print(f'Sending missing list! There is {len(missed_urls)} missing files!')
-
-        return missed_urls
-
-    def download_missed_files(self):
-        """This function checks to see if any files were missed while downloading. If there are none, it terminates and the 
-        next part of the program begins"""
-        try:
-            missed_urls = self.check_files_downloaded()
-            print(f'Checked Downloads Folder! There is {len(missed_urls)} missing files!')
-            if len(missed_urls) == 0:
-                return None  
-        except TypeError:
-            return None
-
-
-        driver = self.OBI_Login()      
-
-        for URL in missed_urls:
-            driver.get(URL)
-            export_xpath = '/html/body/div[2]/div/table[1]/tbody/tr[1]/td[2]/div/table[1]/tbody/tr/td[2]/div/table/tbody/tr/td/div/table/tbody/tr/td[7]'
-            WebDriverWait(driver, 300).until(expected_conditions.element_to_be_clickable((By.XPATH, export_xpath))).click()
-
-            if 'Anakkar' in URL:
-                    excel_xpath = '/html/body/div[3]/table/tbody/tr[1]/td[1]/a[2]/table'
-                    driver.find_element(By.XPATH, excel_xpath).click()
-
-            else:
-                data_xpath = '/html/body/div[3]/table/tbody/tr[1]/td[1]/a[5]/table/tbody/tr/td[2]'
-                driver.find_element(By.XPATH, data_xpath).click()
-                csv_xpath = '/html/body/div[4]/table/tbody/tr[1]/td[1]/a[2]/table/tbody/tr/td[2]'
-                driver.find_element(By.XPATH, csv_xpath).click()
         
 
-            time.sleep(30)
-        driver.quit()
-        return None
 
-    def login_download_check_redownload(self):
-        self.OBI_Login()
-        self.download_files()
-        print('First Sweep Done!')
-        self.check_files_downloaded()
-        self.download_missed_files()
+
 
